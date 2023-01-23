06c337998e262bfe16dcdf46a64825e02f2622b4
Hani Koussa <hankoussa@gmail.com>
2023-01-23 11:52:09 +0000

    Create 17 files

---
+++ build/precache-manifest.06a33c863a6b92ff2183a6287719272b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d05ff9331a509cbc8cf64c63d59dde77",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a73b1c7fadd44ef984f4",
+    "url": "/static/css/2.e1f817c4.chunk.css"
+  },
+  {
+    "revision": "12bac96cc06278b07829",
+    "url": "/static/css/main.d8799457.chunk.css"
+  },
+  {
+    "revision": "a73b1c7fadd44ef984f4",
+    "url": "/static/js/2.1512f2b3.chunk.js"
+  },
+  {
+    "revision": "3bf1c66e928cc0041fec138534da2504",
+    "url": "/static/js/2.1512f2b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12bac96cc06278b07829",
+    "url": "/static/js/main.f9abc25b.chunk.js"
+  },
+  {
+    "revision": "bd0c364045fa373976e8",
+    "url": "/static/js/runtime-main.ba84806c.js"
+  },
+  {
+    "revision": "72e8c96acdbc62c57a2bfb3f689c80a8",
+    "url": "/static/media/herfah.72e8c96a.jpeg"
+  },
+  {
+    "revision": "debddf8698f276e2e9e2f005bab8e991",
+    "url": "/static/media/peakpx.debddf86.jpeg"
+  }
+]);
